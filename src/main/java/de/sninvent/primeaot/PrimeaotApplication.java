547f7aa15a85a3a27675f28d0e286e37b139664d
Soeren Kleinhaus <soeren.kh97@gmail.com>
2024-03-19 09:58:55 +0100

    Initial Commit

---
+++ src/main/java/de/sninvent/primeaot/PrimeaotApplication.java
@@ -0,0 +1,13 @@
+package de.sninvent.primeaot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeaotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeaotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sninvent/primeaot/PrimeaotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sninvent.primeaot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeaotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
